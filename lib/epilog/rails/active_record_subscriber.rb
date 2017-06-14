28e6700cfd33b8089c008fdeb9acb1fcd7b566e4
Justin Howard <jmhoward0@gmail.com>
2017-06-14 11:27:17 -0700

    Fix ActiveRecord subscriber for Rails 5

--- lib/epilog/rails/active_record_subscriber.rb
+++ lib/epilog/rails/active_record_subscriber.rb
@@ -15,7 +15,7 @@ module Epilog
         debug(
           message: payload[:name],
           sql: payload[:sql],
-          binds: binds_info(payload[:binds]),
+          binds: binds_info(payload[:binds] || []),
           metrics: metrics(event)
         )
       end
@@ -29,15 +29,33 @@ module Epilog
       end
 
       def binds_info(binds)
-        binds.map do |column, value|
-          info = { type: column.type, name: column.name }
-          if column.binary?
-            info[:bytes] = value.bytesize
+        binds.map do |bind|
+          if bind.is_a?(Array)
+            bind_column_info(*bind)
           else
-            info[:value] = value
+            bind_attr_info(bind)
           end
-          info
         end
+      end
+
+      def bind_column_info(column, value)
+        info = { type: column.type, name: column.name }
+        if column.binary?
+          info[:bytes] = value.bytesize
+        else
+          info[:value] = value
+        end
+        info
+      end
+
+      def bind_attr_info(attr)
+        info = { type: attr.type.type, name: attr.name }
+        if attr.type.binary? && attr.value
+          info[:bytes] = attr.value_for_database.to_s.bytesize
+        else
+          info[:value] = attr.value_for_database
+        end
+        info
       end
     end
   end
