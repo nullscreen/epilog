50c420a48c292f95dc51fdc9407175af8b53d39a
Wei-Ping Liao <wei-ping.liao@parentsquare.com>
2026-02-09 11:58:19 -0800

    Only patch broadcast method when < 7.1

--- lib/epilog/rails/ext/active_support_logger.rb
+++ lib/epilog/rails/ext/active_support_logger.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-module ActiveSupport
-  class Logger
-    # Rails uses this method to attach additional loggers to the main
-    # Rails.logger object when using the rails console or server. This results
-    # in extra unformatted log output in those cases. Prevent that by
-    # overriding the method with a stub. Examples can be found in
-    #
-    # - railties/lib/rails/commands/server.rb
-    # - active_record/lib/active_record/railtie.rb
-    def self.broadcast(*_args)
-      Module.new
+if ::Rails.gem_version < Gem::Version.new('7.1')
+  module ActiveSupport
+    class Logger
+      # Rails uses this method to attach additional loggers to the main
+      # Rails.logger object when using the rails console or server. This results
+      # in extra unformatted log output in those cases. Prevent that by
+      # overriding the method with a stub. Examples can be found in
+      #
+      # - railties/lib/rails/commands/server.rb
+      # - active_record/lib/active_record/railtie.rb
+      def self.broadcast(*_args)
+        Module.new
+      end
     end
   end
 end
